80cfabe0915904115baf592ccb6a3ae9aace0396
Yomguithereal <guillaumeplique@gmail.com>
2017-04-25 23:34:36 +0200

    Fixing scripts

--- scripts/debug.py
+++ scripts/debug.py
@@ -7,16 +7,22 @@
 from traph import Traph
 
 # Reading the file for our purpose
-lruTrieFile = open('./scripts/data/lru_trie.dat', 'rb')
+lru_trie_file = open('./scripts/data/lru_trie.dat', 'rb')
+link_store_file = open('./scripts/data/link_store.dat', 'rb')
 
-traph = Traph(lru_trie_file=lruTrieFile)
+traph = Traph(lru_trie_file=lru_trie_file, link_store_file=link_store_file)
 trie = traph.lru_trie
+links = traph.link_store
 
-count = 0
+# count = 0
+
+# for page in trie.pages_iter():
+#     count += 1
+
+# print count
 
 for page in trie.pages_iter():
-    count += 1
+    print page
 
-print count
-
-lruTrieFile.close()
+lru_trie_file.close()
+link_store_file.close()
--- scripts/mongo.py
+++ scripts/mongo.py
@@ -11,9 +11,12 @@ from config import CONFIG
 MONGO = CONFIG['mongo']
 
 # Truncating the file for our purpose
-lruTrieFile = open('./scripts/data/lru_trie.dat', 'wb+')
+lru_trie_file = open('./scripts/data/lru_trie.dat', 'wb+')
+link_store_file = open('./scripts/data/link_store.dat', 'wb+')
 
-traph = Traph(lru_trie_file=lruTrieFile)
+traph = Traph(lru_trie_file=lru_trie_file, link_store_file=link_store_file)
+trie = traph.lru_trie
+links = traph.link_store
 
 # Reading from mongo
 client = MongoClient(MONGO['host'], MONGO['port'])
@@ -31,4 +34,5 @@ for page in collection.find({}, {'lru': 1, 'lrulinks': 1}, sort=[("_job", 1)]):
     # for link in page['lrulinks']:
     #     traph.add_page(link)
 
-lruTrieFile.close()
+lru_trie_file.close()
+link_store_file.close()
