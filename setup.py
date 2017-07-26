6a4107a5d771282e69b89c79da754fad0ff90d33
Yomguithereal <guillaumeplique@gmail.com>
2017-07-26 15:49:53 +0200

    Fixing setup.py?

--- setup.py
+++ setup.py
@@ -5,5 +5,10 @@ setup(name='hyphe-traph',
       description='A Trie/Graph hybrid memory structure used by the Hyphe crawler to index pages & webentities.',
       url='http://github.com/medialab/hyphe-traph',
       license='MIT',
-      packages=['traph'],
+      packages=[
+        'traph',
+        'traph.link_store',
+        'traph.lru_trie',
+        'traph.storage'
+      ],
       zip_safe=True)
