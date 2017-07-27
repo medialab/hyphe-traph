f1f2fbcebe3ee6b9b4c680d66a7b0a4f1e9d6d43
Yomguithereal <guillaumeplique@gmail.com>
2017-07-27 18:13:21 +0200

    Testing hugo_links

--- test/__init__.py
+++ test/__init__.py
@@ -9,5 +9,5 @@ from test.suites.traph_test import TestTraph
 from test.suites.network_test import TestNetwork
 from test.suites.twitter_family_test import TestTwitterFamily
 from test.suites.creation_rules_test import TestCreationRules
-# from test.suites.hugo_links_test import TestHugoLinks
+from test.suites.hugo_links_test import TestHugoLinks
 from test.suites.webentities_test import TestWebentities
