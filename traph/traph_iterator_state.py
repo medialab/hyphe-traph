7e1df355f7afa1622ca0d83fab83d1b9f1a88734
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2017-08-30 17:44:14 +0200

    oups forgot new file for TraphIteratorState

---
+++ traph/traph_iterator_state.py
@@ -0,0 +1,28 @@
+# =============================================================================
+# Traph Iterator State Class
+# =============================================================================
+#
+#
+
+
+class TraphIteratorState(object):
+
+    def __init__(self):
+        self.done = False
+        self.result = None
+        self.n_iterations = 0
+
+    def should_yield(self, yield_frequency=10000):
+        self.n_iterations += 1
+        return not self.n_iterations % yield_frequency
+
+    def finalize(self, result):
+        self.done = True
+        self.result = result
+        return self
+
+
+def run_iterator(iterator):
+    for state in iterator:
+        pass
+    return state.result
