d8bdf192765f7fdd5e90a575537f00b925561e51
Yomguithereal <guillaumeplique@gmail.com>
2019-02-06 16:31:08 +0100

    Bump 1.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='hyphe-traph',
-      version='1.0.0',
+      version='1.1.0',
       description='A Trie/Graph hybrid memory structure used by the Hyphe crawler to index pages & webentities.',
       url='http://github.com/medialab/hyphe-traph',
       license='MIT',
