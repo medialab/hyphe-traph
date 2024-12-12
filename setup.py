62991744dd09192f186900ac2abc48125dd33590
Yomguithereal <guillaumeplique@gmail.com>
2024-12-12 11:09:27 +0100

    Bump 3.0.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     description="A Trie/Graph hybrid memory structure used by the Hyphe crawler to index pages & webentities.",
     url="http://github.com/medialab/hyphe-traph",
     license="MIT",
-    author="Guillaume Plique",
+    author="Guillaume Plique, Béatrice Mazoyer",
     packages=["traph", "traph.link_store", "traph.lru_trie", "traph.storage"],
     zip_safe=True,
 )
